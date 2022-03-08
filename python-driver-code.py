61a1309878fd394277fe51beb62d7a82cf39e66c
John1996son <89975577+John1996son@users.noreply.github.com>
2022-03-08 18:36:02 +0530

    Update python-driver-code.py

--- python-driver-code.py
+++ python-driver-code.py
@@ -1,9 +1,22 @@
 # Python Driver Code
 
 def solve(s: str) -> chr:
-  # Your code goes here
-  # s is the given input string
-  return 
+  # l = len(s)
+  sec_count=0
+  count=0
+  
+  for i in range(l):
+    
+    cur_count=1
+    for j in range(i+1,l):
+      if (s[i]=!s[j]):
+          break
+          cur_count+=1
+          
+          if cur_count > count :
+          sec_count = count
+          count = cur_count
+  return sec_count
 
 # The following snippet reads the input in the required format. 
 # Just complete the solve function above. 
